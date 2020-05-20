202d5eb072652c9126a4d0e5468d918cf598b9ce
touchy <prashantgecg@gmail.com>
2020-05-20 15:28:15 +0530

    githubcontect finder 2020

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Layout from "./Layout";
-import { Route, Switch } from "react-router-dom";
-import Home from "../components/pages/Home/Home";
-import About from "../components/pages/About/About";
-import UserDetail from "../components/pages/UserDetail/UserDetail";
+// import { Route, Switch } from "react-router-dom";
+// import Home from "../components/pages/Home/Home";
+// import About from "../components/pages/About/About";
+// import UserDetail from "../components/pages/UserDetail/UserDetail";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -12,11 +12,11 @@ const App = () => {
     <Layout>
       <div>App: {process.env.GITHUB_CLIENT_ID}</div>
       REACT {process.env.REACT_APP_CLIENT_ID}
-      <Switch>
+      {/* <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
         <Route path="/user/:id" exact component={UserDetail} />
-      </Switch>
+      </Switch> */}
     </Layout>
   );
 };
