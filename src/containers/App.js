027fa5fcd3a392181036b40949570940435e421d
touchy <prashantgecg@gmail.com>
2020-05-20 16:09:33 +0530

    githubcontect finder 2020

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,8 +10,6 @@ import "./App.css";
 const App = () => {
   return (
     <Layout>
-      <div>App GITHUB_CLIENT_ID: {process.env.GITHUB_CLIENT_ID}</div>
-      <div>App: {process.env.REACT_APP_API_KEY}</div>
       <div>REACT: {process.env.REACT_APP_CLIENT_ID}</div>
       <div>New2 : {process.env.REACT_APP_SECRET}</div>
       {/* <Switch>
